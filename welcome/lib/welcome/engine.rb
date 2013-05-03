0596b1dad336303a606f95a3e48eada9c298a974
Tom McKay <thomasmckay@redhat.com>
2013-05-03 13:49:43 -0400

    navigation - nested nav

--- welcome/lib/welcome/engine.rb
+++ welcome/lib/welcome/engine.rb
@@ -1,4 +1,9 @@
 module Welcome
   class Engine < ::Rails::Engine
+    initializer :finisher_hook do |engine|
+      resources = Dir[File.dirname(__FILE__) + '/navigation/*.rb']
+      resources.uniq.each{ |f| require f }
+      ::Experimental::Navigation::Additions.insert_before(:dashboard, Welcome::Navigation::Welcome)
+    end
   end
 end
